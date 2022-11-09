8f2b56b512324d4efc1ded46c1cc852eb6379c5f
Guile Liz <117557290+GuileLiz@users.noreply.github.com>
2022-11-09 21:18:00 +0800

    Add files via upload

---
+++ city_names.py
@@ -0,0 +1,5 @@
+def city_country(city,country):
+	"""Disply the information of country and city"""
+	city_country = print(f"{city.title()}, {country.title()}")
+	return city_country
+city_country('manila','Philippines')
