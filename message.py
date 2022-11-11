acda281fbe005730df81f9e3cabbb72216e3dd56
Guile Liz <117557290+GuileLiz@users.noreply.github.com>
2022-11-11 19:30:25 +0800

    Add files via upload

---
+++ message.py
@@ -0,0 +1,11 @@
+def display_message():
+	"""Display  message"""
+	print('I am learning how to use function')
+
+display_message()
+
+def fav_book(book):
+	"""Disply fav book"""
+	print(f"My fav book is {book.title()}")
+
+fav_book('Alice in wonderland')
