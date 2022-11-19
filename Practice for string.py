aa4a25f7acf282dbd132685027670a4b1eea5d9c
Guile Liz <117557290+GuileLiz@users.noreply.github.com>
2022-11-19 23:22:52 +0800

    Add files via upload

---
+++ Practice for string.py
@@ -0,0 +1,11 @@
+name = "Guile"
+print(f"Hello {name}, would you like to learn python?")
+print(f"Hello {name.upper()}, would you like to learn python?")
+print(f"Hello {name.lower()}, would you like to learn python?")
+quote = "consistency is the key"
+print(f"{name.title()} once said, {quote}")
+name = "  Guile \n \t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
